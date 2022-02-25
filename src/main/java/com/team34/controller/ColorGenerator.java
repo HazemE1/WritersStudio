77b2e819baae626cb308d4b08c7911a986b49b9a
fridajac <frida_jacobsson@live.se>
2022-02-25 15:06:40 +0100

    New ColorGenerator class

---
+++ src/main/java/com/team34/controller/ColorGenerator.java
@@ -0,0 +1,28 @@
+package com.team34.controller;
+
+import java.util.Random;
+
+public class ColorGenerator {
+
+    static Random rand = new Random();
+
+    public static String getNewColor() {
+
+        switch (rand.nextInt(4)+1) {
+            case 1:
+                return "#21AFBF";
+            case 2:
+                return "#A0D9D9";
+            case 3:
+                return "#F2D9BB";
+            case 4:
+                return "#F2C3A7";
+            case 5:
+                return "#F2E0D0";
+            case 6:
+                return "#2179BF";
+            default:
+                return "#F282A7";
+        }
+    }
+}
