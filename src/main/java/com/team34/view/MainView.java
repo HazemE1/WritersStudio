055b2d10794188167ee22c13720cd029fc74f9f1
fridajac <frida_jacobsson@live.se>
2022-03-01 12:01:42 +0100

    Merged colorBranch to master and fixed small error

--- src/main/java/com/team34/view/MainView.java
+++ src/main/java/com/team34/view/MainView.java
@@ -448,7 +448,7 @@ public class MainView {
         timeline.clear();
         if (events != null) {
             for (int i = 0; i < events.length; i++) {
-                timeline.addEvent((Long) events[i][0], (String) events[i][1]);
+                timeline.addEvent((Long) events[i][0], (String) events[i][1], (String) events[i][3]);
             }
         }
         if (eventOrder != null)
